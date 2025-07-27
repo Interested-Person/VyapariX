80e15cdf297cf7cecedce80204f049f6d1e6a213
Pritam2707 <priyamgamer0@gmail.com>
2025-07-27 21:07:58 +0530

    fix flex

--- vyaparix/src/pages/Merchant.tsx
+++ vyaparix/src/pages/Merchant.tsx
@@ -5,32 +5,35 @@ import ProductCard2 from "../Components/Modals/ProductCard2";
 import { useAuth } from "../hooks/useAuth";
 const Merchant = () => {
     const { selfProducts } = useMerchant();
-    const {user}=useAuth()
+    const { user } = useAuth()
     const navigate = useNavigate()
     return (
         <div className="min-h-screen p-4 flex flex-col gap-10 bg-teal-950">
-            <h1 className="text-2xl text-white">Welcome {user?.username}<br/>Your mechant id is {user?.uid}</h1>
+            <h1 className="text-2xl text-white">Welcome {user?.username}<br />Your mechant id is {user?.uid}</h1>
             <h2 className="text-xl text-white">Your options</h2>
             <div className="flex gap-10  ">
-            <button className="bg-teal-400 p-4 rounded-md" onClick={() => navigate("/addproduct")}>Add new product</button>
-            <button className="bg-teal-400 p-4 rounded-md" >Check revenue</button>
-            <button className="bg-teal-400 p-4 rounded-md" >Check pending Orders</button>
+                <button className="bg-teal-400 p-4 rounded-md" onClick={() => navigate("/addproduct")}>Add new product</button>
+                <button className="bg-teal-400 p-4 rounded-md" >Check revenue</button>
+                <button className="bg-teal-400 p-4 rounded-md" >Check pending Orders</button>
 
             </div>
             <h2 className="text-xl text-white">Currently Listed Products</h2>
             <div className="flex flex-wrap">
-
-                {selfProducts.map((p) => {
-                    return (
-                        <div className="grow basis-1/4 my-4" key={p.docID}>
-                            {/* <ProductCard product={p} /> */}
-                            <ProductCard2  docID={p?.docID ? p.docID : ""} product={p} isMerchantPage={true} />
-
-                        </div>
-                    )
-                })}
-
+                {selfProducts.map((p) => (
+                    <div
+                        className="flex-none basis-1/4 my-4"
+                        key={p.docID}
+                    >
+                        <ProductCard2
+                            docID={p?.docID || ""}
+                            product={p}
+                            isMerchantPage={true}
+                        />
+                    </div>
+                ))}
             </div>
+
+
 
         </div>
 
