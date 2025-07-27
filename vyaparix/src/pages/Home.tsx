14369c5915e3b008f9933b2e0c361def73f6d5d8
Pritam2707 <priyamgamer0@gmail.com>
2025-07-27 21:25:00 +0530

    Home

--- vyaparix/src/pages/Home.tsx
+++ vyaparix/src/pages/Home.tsx
@@ -1,7 +1,25 @@
+import ProductCard2 from "../Components/Modals/ProductCard2"
+import { useProducts } from "../hooks/useProducts"
+
 const Home = () => {
+    const { products } = useProducts()
     return (
-        <div className="bg-emerald-50 w-screen h-screen">
-            <h1 >Home</h1>
+        <div className="bg-teal-950 flex flex-col gap-4 w-screen min-h-screen">
+            <h1 className="text-xl m-4 mx-auto text-white">Recently Added</h1>
+            <div className="flex flex-wrap">
+                {products.map((p) => (
+                    <div
+                        className="flex-none basis-1/4 my-4"
+                        key={p.docID}
+                    >
+                        <ProductCard2
+                            docID={p?.docID || ""}
+                            product={p}
+                            isMerchantPage={true}
+                        />
+                    </div>
+                ))}
+            </div>
 
 
         </div>
